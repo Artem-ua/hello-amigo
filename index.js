ba80ffb13e39ad0aaf6001a03ea965fa4e8a2b47
Artem Kushnir <artemy.kushnir@gmail.com>
2023-07-01 20:39:30 +0300

    Second endpoint added

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/deniss', (req, res) => {
+    res.send('Hello Deniss!')
+  })
+
 app.listen(3000, () => {
   console.log('Приклад застосунку, який прослуховує 3000-ий порт!')
 })
-
-
-
